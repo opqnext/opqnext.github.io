3f1da5bc23673c2b359838f6f07110726b7ccfaa
opqnext <explame@opqnext.com>
2026-05-19 16:26:15 +0800

    deploy blog 2026-05-19 16:26

--- deploy-blog.sh
+++ deploy-blog.sh
@@ -2,7 +2,16 @@
 set -e
 
 cd /Users/momo/data/opqnext/anthropic/website/blog-src
+hexo clean
 hexo generate
+
+# Compile polarbear theme SCSS to CSS
+SASS_CMD=$(command -v sass || command -v npx)
+if command -v sass &> /dev/null; then
+    sass /Users/momo/data/opqnext/anthropic/website/blog-src/themes/polarbear/source/css/style.scss /Users/momo/data/opqnext/anthropic/website/blog/css/style.css
+elif command -v npx &> /dev/null; then
+    npx -y sass /Users/momo/data/opqnext/anthropic/website/blog-src/themes/polarbear/source/css/style.scss /Users/momo/data/opqnext/anthropic/website/blog/css/style.css
+fi
 
 cd /Users/momo/data/opqnext/anthropic/website
 git add -A
